1fb6702b664f82f533309d7ec0a715a3333cd454
BranPap <branpap@stanford.edu>
2026-01-26 14:23:56 -0800

    removed debug

--- experiment.js
+++ experiment.js
@@ -14,7 +14,7 @@ const jsPsych = initJsPsych({
   auto_update_progress_bar: false,
   on_finish: function(data) {
     // proliferate.submit({ trials: data.values() });
-    jsPsych.data.displayData('csv');
+    // jsPsych.data.displayData('csv');
   }
 });
 
